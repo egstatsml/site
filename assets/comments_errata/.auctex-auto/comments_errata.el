e056f3274247dc2d7ff0901fbd71d01c3d4dcdc4
Ethan Goan <ethanjgoan@gmail.com>
2026-03-31 23:26:15 +1000

    dev: update

--- assets/comments_errata/.auctex-auto/comments_errata.el
+++ assets/comments_errata/.auctex-auto/comments_errata.el
@@ -6,7 +6,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("babel" "english") ("geometry" "letterpaper" "top=0.5cm" "bottom=0.5cm" "left=1cm" "right=1cm" "marginparwidth=1.75cm") ("amsmath" "") ("graphicx" "") ("xcolor" "") ("hyperref" "colorlinks=true" "allcolors=blue") ("titlesec" "") ("tocloft" "") ("sectsty" "")))
+                     '(("fancyhdr" "") ("babel" "english") ("geometry" "letterpaper" "top=0.5cm" "bottom=0.5cm" "left=1cm" "right=1cm" "marginparwidth=1.75cm" "bottom=2cm" "bottom=3cm" "includeheadfoot" "bottom=1cm") ("amsmath" "") ("graphicx" "") ("xcolor" "") ("hyperref" "colorlinks=true" "allcolors=blue") ("titlesec" "") ("tocloft" "") ("sectsty" "")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
